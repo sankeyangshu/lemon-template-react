439636528ed38563f5ff0802252458175f0a0399
sankeyangshu <1260323835@qq.com>
2025-11-10 15:56:07 +0800

    feat: add new icon route component with icon display functionality

---
+++ src/pages/example/icon.tsx
@@ -0,0 +1,54 @@
+import { createFileRoute, useRouter } from '@tanstack/react-router';
+import { useTranslation } from 'react-i18next';
+import NavBar from '@/components/custom/nav-bar';
+import SvgIcon from '@/components/custom/svg-icon';
+
+export const Route = createFileRoute('/example/icon')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  const iconifyList = [
+    'mdi:github',
+    'mdi:palette',
+    'mdi:book-open-variant',
+    'mdi:cellphone-settings-variant',
+    'mdi:logout',
+  ];
+
+  return (
+    <>
+      <NavBar
+        title={t('router.icon')}
+        leftArrow={(
+          <SvgIcon className="text-2xl" icon="mdi:chevron-left" />
+        )}
+        onClickLeft={() => void router.history.back()}
+      />
+      <div className="box-border w-full p-4">
+        <div className="mb-3 border-l-3 border-solid border-l-primary pl-3 leading-7">
+          <div className="my-1 text-lg font-bold">Iconify Icon</div>
+        </div>
+        <div className="flex">
+          {iconifyList.map((item) => (
+            <div key={item}>
+              <SvgIcon icon={item} className="mr-2.5 text-2xl" />
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-4 mb-3 border-l-3 border-solid border-l-primary pl-3 leading-7">
+          <div className="my-1 text-lg font-bold">Local SVG Icon</div>
+        </div>
+        <div className="flex">
+          <div className="mr-2.5 text-2xl">
+            <SvgIcon localIcon="logo" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
